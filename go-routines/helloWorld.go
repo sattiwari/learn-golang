786eeefcd29e35e78fa841ff9e361023776284c4
Satendra Tiwari <sattiwari18@gmail.com>
2016-10-20 17:38:01 +0700

    ensure go routines run entirely

--- go-routines/helloWorld.go
+++ go-routines/helloWorld.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "fmt"
+  "sync"
   "time"
 )
 
@@ -11,15 +12,20 @@ type Job struct {
   text string
 }
 
-func outputText(j *Job) {
+func outputText(j *Job, goGroup *sync.WaitGroup) {
   for j.i < j.max {
     time.Sleep(1 * time.Millisecond)
     fmt.Println(j.text)
     j.i++
   }
+  goGroup.Done()
 }
 
 func main() {
+
+  goGroup := new(sync.WaitGroup)
+  fmt.Println("Starting")
+
   hello := new(Job)
   world := new(Job)
   
@@ -31,8 +37,12 @@ func main() {
   world.i = 0
   world.max = 5
 
-  outputText(hello)
-  outputText(world)
+  go outputText(hello, goGroup)
+  go outputText(world, goGroup)  
+
+  goGroup.Add(2)
+  goGroup.Wait()
+  
 }
 
 
